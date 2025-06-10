df5bc4fd9904685c9e45e71dd0dd1104ab93b177
shunsuke-anzai <a35112865@gmail.com>
2025-06-10 19:33:34 +0900

    フロントベース作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
-import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import ClientLayout from "@/components/ClientLayout";
-//import ClientLayout from '../components/ClientLayout';
+import { ReactNode } from 'react';
+import Providers from '@/components/Providers';
 
-export const metadata: Metadata = {
-  title: "Unite Court Reservation App",
-  description: "ユナイトコート予約システム",
+export const metadata = {
+  title: 'カレンダー',
+  icons: { icon: '/favicon.ico' },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html>
+    <html lang="ja">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.8/main.min.css"
+          rel="stylesheet"
+        />
+      </head>
       <body>
-        <ClientLayout>
-          {children}    
-        </ClientLayout>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
